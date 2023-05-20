3cebec5561ceb721ab267e246f5dd22c773f59f4
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-20 23:50:07 +0700

    Performed some modifications

--- calculator_class.py
+++ calculator_class.py
@@ -9,17 +9,15 @@ class Calculator:
     #under the Calculator class, create a method for the menu
     def menu(self):
         while True:
+            print("\nOPTIONS: \n\t[1] Addition, \n\t[2] Subtraction, \n\t[3] Multiplication, \n\t[4] Division")
             try:
                 #under the menu method, ask the user what operation he/she wants to use
-                print("OPTIONS: \n\t[1] Addition, \n\t[2] Subtraction, \n\t[3] Multiplication, \n\t[4] Division")
-                operation = int(input("\nKindly enter the number that corresponds to the operation you want to perform: "))
-                #if the input is among number 1-4,
+                operation = int(input("\tKindly enter the number that corresponds to the operation you want to perform: "))
+                #if the input is among number 1-4, then return operation
                 if operation in range (1,5):
-                    #then return operation
                     return operation
-                #else, if the input is not belong to numbers from 1-4,
+                #else, if the input is not belong to numbers from 1-4, then notify the user
                 else:
-                    #then
                     print("\n\tInvalid Input. Please enter a number from 1-4 only.")
             #if there's an error detected, except block will be executed
             except (ValueError, TypeError):
@@ -31,7 +29,7 @@ class Calculator:
             try:
                 #under the user input method, ask user for the first number
                 first_number = float(input("\n\tEnter first number: "))
-                #under the user input method, ask user for the second number
+                #ask user for the second number
                 second_number = float(input("\tEnter second number: "))
                 #return the first and second inputs
                 return (first_number, second_number)
@@ -54,7 +52,6 @@ class Calculator:
         #under the subtraction method, call the get_numbers method to get the inputs from user that are needed to perform the operation
         inputs = self.get_numbers()
         #perform the subtraction operation on two numbers.
-        #the 1st number is inputs[0] and the 2nd number is inputs[1]
         difference = inputs[0] - inputs[1]
         #print the difference of the two numbers
         print("\n\tThe difference is " + str(difference))
@@ -64,7 +61,6 @@ class Calculator:
         #under the multiplication method, call the get_numbers method to get the inputs from user that are needed to perform the operation
         inputs = self.get_numbers()
         #perform the multiplication operation on two numbers.
-        #the 1st number is inputs[0] and the 2nd number is inputs[1]
         product = inputs[0] * inputs[1]
         #print the product of the two numbers
         print("\n\tThe product is " + str(product))
@@ -76,7 +72,6 @@ class Calculator:
                 #under the division method, call the get_numbers method to get the inputs from user that are needed to perform the operation
                 inputs = self.get_numbers()
                 #perform the division operation on two numbers.
-                #the 1st number is inputs[0] and the 2nd number is inputs[1]
                 quotient = inputs[0] / inputs[1]
                 #print the quotient of the two numbers
                 if quotient < 1:
@@ -86,3 +81,5 @@ class Calculator:
             #if there's an error detected, except block will be executed
             except ZeroDivisionError:
                 print("\n\tZero Division Error: Second number cannot be 0.")
+            else:
+                break
--- main_program.py
+++ main_program.py
@@ -3,4 +3,4 @@ from calculator_class import Calculator
 calculate = Calculator()
 
 calculate.menu()
-
+calculate.division()
