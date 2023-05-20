bde4eae5dc7e21e84ead344100e96719434e5835
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-21 00:08:46 +0700

    Menu method and Get numbers method were successfully called within the main program

--- main_program.py
+++ main_program.py
@@ -2,8 +2,12 @@
 from calculator_class import Calculator
 
 #construct an object named simple_calculator
+simple_calculator = Calculator()
 #by calling the 'menu method', diplay the menu and ask the user what operation he/she wants to use
+simple_calculator.menu()
 #ask the user for two numbers
+simple_calculator.get_numbers()
+
 #if the user enters number 1, call the 'addition method'
 #if the user enters number 2, call the 'subtraction method'
 #if the user enters number 3, call the 'multiplication method'
