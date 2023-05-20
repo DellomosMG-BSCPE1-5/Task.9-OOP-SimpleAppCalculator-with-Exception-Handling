ff7f5b28b2f766c5ec132fdae11a0adafe19a49d
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-21 00:14:11 +0700

    Calling addition method. Done

--- main_program.py
+++ main_program.py
@@ -4,11 +4,11 @@ from calculator_class import Calculator
 #construct an object named simple_calculator
 simple_calculator = Calculator()
 #by calling the 'menu method', diplay the menu and ask the user what operation he/she wants to use
-simple_calculator.menu()
-#ask the user for two numbers
-simple_calculator.get_numbers()
+operation = simple_calculator.menu()
 
 #if the user enters number 1, call the 'addition method'
+if operation == 1:
+    simple_calculator.addition()
 #if the user enters number 2, call the 'subtraction method'
 #if the user enters number 3, call the 'multiplication method'
 #if the user enters number 4, call the 'division method'
