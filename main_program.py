c1d4ee94023ca023b63b845047778f2928fa15b3
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-21 00:04:44 +0700

    Pseudocode for the main program

--- main_program.py
+++ main_program.py
@@ -1,4 +1,12 @@
+#importing the Calculator class into the main_program file
 from calculator_class import Calculator
 
-calculate = Calculator()
+#construct an object named simple_calculator
+#by calling the 'menu method', diplay the menu and ask the user what operation he/she wants to use
+#ask the user for two numbers
+#if the user enters number 1, call the 'addition method'
+#if the user enters number 2, call the 'subtraction method'
+#if the user enters number 3, call the 'multiplication method'
+#if the user enters number 4, call the 'division method'
+#after using the program, ask the user if he/she wants to use the program again
 
