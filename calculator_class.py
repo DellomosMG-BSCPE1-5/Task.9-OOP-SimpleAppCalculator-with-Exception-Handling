59afce270318cbee46b64d3614f89252a38e5c46
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-21 12:12:10 +0700

    EXTRA added

--- calculator_class.py
+++ calculator_class.py
@@ -1,19 +1,19 @@
 #Redo calculator program applying the OOP concept.
 
-#pseudocode for the class file
-#create a file for the calculator class that contains necessary methods
-
+#import the UserInterface Class
+from user_interface import UserInterface
+input = UserInterface()
 #for the class file, create a class named Calculator
 class Calculator:
 
     #constructor
     def __init__(self):
         pass
-    
+
     #create method for addition operation
     def addition(self):
         #under the addition method, call the get_numbers method to get the inputs from user that are needed to perform the operation
-        inputs = self.get_numbers()
+        inputs = input.get_numbers()
         #perform the addition operation on two numbers.
         #the 1st number is inputs[0] and the 2nd number is inputs[1]
         sum = inputs[0] + inputs[1]
@@ -23,7 +23,7 @@ class Calculator:
     #create method for subtraction operation
     def subtraction(self):   
         #under the subtraction method, call the get_numbers method to get the inputs from user that are needed to perform the operation
-        inputs = self.get_numbers()
+        inputs = input.get_numbers()
         #perform the subtraction operation on two numbers.
         difference = inputs[0] - inputs[1]
         #print the difference of the two numbers
@@ -32,7 +32,7 @@ class Calculator:
     #create method for multiplication operation
     def multiplication(self):
         #under the multiplication method, call the get_numbers method to get the inputs from user that are needed to perform the operation
-        inputs = self.get_numbers()
+        inputs = input.get_numbers()
         #perform the multiplication operation on two numbers.
         product = inputs[0] * inputs[1]
         #print the product of the two numbers
@@ -43,7 +43,7 @@ class Calculator:
         while True:
             try:
                 #under the division method, call the get_numbers method to get the inputs from user that are needed to perform the operation
-                inputs = self.get_numbers()
+                inputs = input.get_numbers()
                 #perform the division operation on two numbers.
                 quotient = inputs[0] / inputs[1]
                 #print the quotient of the two numbers
--- user_interface.py
+++ user_interface.py
@@ -19,10 +19,10 @@ class UserInterface:
                 )
             )
 
-    #menu
+    #menu method
     def menu(self):
         while True:
-            print(Fore.CYAN + Style.BRIGHT + "OPTIONS:", Fore.WHITE + Style.NORMAL + "\n[1] Addition, \n[2] Subtraction, \n[3] Multiplication, \n[4] Division")
+            print(Fore.CYAN + Style.NORMAL + "OPTIONS:", Fore.WHITE + Style.NORMAL + "=" * 15, "\n[1] Addition \n[2] Subtraction \n[3] Multiplication \n[4] Division\n" + "=" * 24)
             try:
                 #under the menu method, ask the user what operation he/she wants to use
                 print(Fore.LIGHTCYAN_EX + Style.NORMAL + "\nKindly enter the number that corresponds to the operation you want to perform: ", end = "")
@@ -32,11 +32,23 @@ class UserInterface:
                     return operation
                 #else, if the input is not belong to numbers from 1-4, then notify the user
                 else:
-                    print("\n\tInvalid Input. Please enter a number from 1-4 only.")
+                    print('\n' +
+                            boxen(
+                                "  Invalid Input. Please enter a number from 1-4 only. ",
+                                color="cyan",
+                                padding = (0,7,0,7)
+                            )
+                        )                 
             #if there's another error detected, except block will be executed
             except (ValueError, TypeError):
-                print("\n\tInvalid Input. Please enter a numeric character.")
-    
+                print('\n' +
+                        boxen(
+                            "  Invalid Input. Please enter a numeric character. ",
+                            color="cyan",
+                            padding = (0,7,0,7)
+                        )
+                    )       
+                  
     #method for the user input
     def get_numbers(self):
         while True:
@@ -49,4 +61,10 @@ class UserInterface:
                 return (first_number, second_number)
             #if there's an error detected, except block will be executed
             except (ValueError, TypeError):
-                print("\n\tInvalid Input. Please enter a numeric character.")
+                print('\n' +
+                        boxen(
+                            "  Invalid Input. Please enter a numeric character. ",
+                            color="cyan",
+                            padding = (0,7,0,7)
+                        )
+                    )       
